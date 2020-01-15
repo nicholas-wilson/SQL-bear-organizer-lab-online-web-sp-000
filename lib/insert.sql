70fd6a5faf82f140ba96287f46b380ce3f0e21d7
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-15 01:05:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Choc
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 23, 'male', 'red', 'calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 9, 'male', 'black', 'calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 13, 'male', 'brown', 'calm', 1);
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES(10, 'male', 'black', 'aggro', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(null, 10, 'male', 'black', 'aggro', 1);
