0c48109b1a34e7fba65b464cf36569cb4cc3fd63
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-15 01:04:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'red', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'red', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'red', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'black', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 15, 'male', 'red', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 14, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 4, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 23, 'male', 'red', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 9, 'male', 'black', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr.Chocolate', 13, 'male', 'brown', 'calm', 1);
 INSERT INTO bears (age, gender, color, temperament, alive) VALUES(10, 'male', 'black', 'aggro', 1);
